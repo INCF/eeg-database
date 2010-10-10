207493400f076b05adb72fb9d402c63079ef4801
pitrs <pitrs@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2010-10-10 12:06:02 +0000

    moved sql
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@587 1a751040-f3c7-4aaa-98da-1e85aacc1d30

---
+++ sql/scripts/fill_database/insert_into_NEWS.sql
@@ -0,0 +1,3 @@
+INSERT INTO NEWS(NEWS_ID, PERSON_ID, RESEARCH_GROUP_ID, MESSAGE, TIME, TITLE)VALUES(1,1,1,'Ve st�edu 30.3.2010 prob�hne sch�zka p��tel EEG.', SYSDATE ,'EEG ve�er' );
+INSERT INTO NEWS(NEWS_ID, PERSON_ID, RESEARCH_GROUP_ID, MESSAGE, TIME, TITLE)VALUES(2,2,2,'Ve st�edu 7.4.2010 neprob�hne sch�zka s EEG-t�mem. D�vod: Jsem nemocn�',SYSDATE,'Zru�en� sch�zky' );
+INSERT INTO NEWS(NEWS_ID, PERSON_ID, RESEARCH_GROUP_ID, MESSAGE, TIME, TITLE)VALUES(3,2,2,'Ve p�tek 9.4.2010 prob�hne u n�s v laborato�i UU-403 �kolen� t�kaj�c� se nov� EEG metody. ��ast povinn� pro v�echny experiment�tory',SYSDATE, '�kolen�' );
