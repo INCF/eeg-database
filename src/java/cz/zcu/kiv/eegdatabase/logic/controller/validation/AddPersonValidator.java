9296e0ecf260cb34263fea17820b7b75591dbd38
stebjan <stebjan@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2010-12-07 16:02:50 +0000

    #137 - pattern updated - diakritic characters problem solved.
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@728 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/java/cz/zcu/kiv/eegdatabase/logic/controller/validation/AddPersonValidator.java
+++ src/java/cz/zcu/kiv/eegdatabase/logic/controller/validation/AddPersonValidator.java
@@ -55,10 +55,10 @@ public class AddPersonValidator implements Validator {
       }
 
     }
-    if (!Pattern.matches("[a-zA-Z][a-zA-Z\\s]*", apc.getGivenname())) {
+    if (!Pattern.matches("[a-zA-Zá-žÁ-Ž][a-zA-Zá-žÁ-Ž\\s]*", apc.getGivenname())) {
       errors.rejectValue("givenname", "invalid.givenname");
     }
-    if (!Pattern.matches("[a-zA-Z][a-zA-Z\\s]*",apc.getSurname())) {
+    if (!Pattern.matches("[a-zA-Zá-žÁ-Ž][a-zA-Zá-žÁ-Ž\\s]*",apc.getSurname())) {
       errors.rejectValue("surname", "invalid.surname");
     }
 
