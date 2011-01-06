8c5d015335a5ba384ec18de9ba0df011a241ddb6
jpergler <jpergler@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2011-01-06 18:53:46 +0000

    Implemented editing of research group.
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@754 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/java/cz/zcu/kiv/eegdatabase/logic/controller/group/CreateGroupController.java
+++ src/java/cz/zcu/kiv/eegdatabase/logic/controller/group/CreateGroupController.java
@@ -1,13 +1,19 @@
 package cz.zcu.kiv.eegdatabase.logic.controller.group;
 
+import cz.zcu.kiv.eegdatabase.data.dao.AuthorizationManager;
+import cz.zcu.kiv.eegdatabase.data.dao.ResearchGroupDao;
 import cz.zcu.kiv.eegdatabase.data.pojo.ResearchGroup;
 import cz.zcu.kiv.eegdatabase.data.service.DataService;
 import cz.zcu.kiv.eegdatabase.logic.util.ControllerUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Jindrich Pergler
@@ -17,10 +23,57 @@ public class CreateGroupController extends SimpleFormController {
     private Log log = LogFactory.getLog(getClass());
     @Autowired
     private DataService dataService;
+    @Autowired
+    private ResearchGroupDao researchGroupDao;
+    @Autowired
+    private AuthorizationManager auth;
 
     public CreateGroupController() {
         setCommandClass(CreateGroupCommand.class);
         setCommandName("createGroup");
+    }
+
+    @Override
+    protected Object formBackingObject(HttpServletRequest request) throws Exception {
+        CreateGroupCommand data = (CreateGroupCommand) super.formBackingObject(request);
+
+        String idString = request.getParameter("groupId");
+
+        if (idString != null) {
+            // Editing existing hardware
+            int id = Integer.parseInt(idString);
+
+            log.debug("Loading research group to the command object for editing.");
+            ResearchGroup researchGroup = researchGroupDao.read(id);
+
+            data.setId(researchGroup.getResearchGroupId());
+            data.setResearchGroupTitle(researchGroup.getTitle());
+            data.setResearchGroupDescription(researchGroup.getDescription());
+        }
+
+        return data;
+    }
+
+    @Override
+    protected ModelAndView showForm(HttpServletRequest request, HttpServletResponse response, BindException errors) throws Exception {
+        ModelAndView mav = super.showForm(request, response, errors);
+
+        String idString = request.getParameter("groupId");
+
+        if (idString != null) {
+            // Editing existing hardware
+            int id = Integer.parseInt(idString);
+            if (!auth.userIsAdminInGroup(id)) {
+                mav.setViewName("redirect:/groups/detail.html?groupId=" + id);
+            }
+        } else {
+            // Creating new group
+            if (!auth.userIsExperimenter()) {
+                mav.setViewName("redirect:/groups/list.html");
+            }
+        }
+
+        return mav;
     }
 
     @Override
@@ -30,11 +83,23 @@ public class CreateGroupController extends SimpleFormController {
 
         CreateGroupCommand data = (CreateGroupCommand) command;
 
-        ResearchGroup group = dataService.createResearchGroup(data.getResearchGroupTitle(),
-                data.getResearchGroupDescription(),
-                ControllerUtils.getLoggedUserName());
+        ResearchGroup researchGroup;
+        if (data.getId() > 0) {
+            // Editing
+            log.debug("Editing existing research group.");
+            researchGroup = researchGroupDao.read(data.getId());
+            researchGroup.setTitle(data.getResearchGroupTitle());
+            researchGroup.setDescription(data.getResearchGroupDescription());
+        } else {
+            // Creating new
+            log.debug("Creating new research group.");
+            researchGroup = dataService.createResearchGroup(data.getResearchGroupTitle(),
+                    data.getResearchGroupDescription(),
+                    ControllerUtils.getLoggedUserName());
+        }
 
-        mav.setViewName("redirect:/groups/detail.html?groupId=" + group.getResearchGroupId());
+
+        mav.setViewName("redirect:/groups/detail.html?groupId=" + researchGroup.getResearchGroupId());
 
         log.debug("Returning MAV.");
         return mav;
