48989842d8504cd33505a2a25b58914363970dd5
jnovotny <jnovotny@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2011-11-10 13:36:48 +0000

    -Updated xsd schema versions in persistence.xml and controllers.xml -Updated test-context.xml schema definitions -Moved project.test.properties to test dir, enabling property override in the test context -Fixed hibernate mapping location def. in test-contex.xml -Fixed dao bean definitions in test-context.xml by adding sessionFactory references -Added a simple configuration test = test class using test-context.xml to initialize with an empty body -3 tests besides that are now succeeding (methods in BookingRoomUtilsTest), 55 still failing
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1055 1a751040-f3c7-4aaa-98da-1e85aacc1d30

---
+++ src/test/java/cz/zcu/kiv/eegdatabase/data/ConfigurationSetupTest.java
@@ -0,0 +1,29 @@
+package cz.zcu.kiv.eegdatabase.data;
+
+import org.junit.Test;
+import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jnovotny
+ * Date: 10.11.11
+ * Time: 12:59
+ * This test verifies that the test-context.xml is not seriously misconfigured
+ * by trying to initialize a test using it.
+ * Body of the test method is empty, it will fail if there is an configuration error
+ * in test-context.xml or linked files (hibernate config, hibernate mapping, project properties)
+ */
+public class ConfigurationSetupTest extends AbstractTransactionalDataSourceSpringContextTests {
+
+	@Test
+	public void testInitializationFromTestContext(){
+		System.out.println("test-context.xml initialization succeeded");
+	}
+
+
+    protected String[] getConfigLocations() {
+        return new String[] {"test-context.xml"};
+        //Uncomment this if you want the test to use production contexts. Warning: Might need additional configuration to work properly
+        //return new String[] {"file:src/main/webapp/WEB-INF/persistence.xml","file:src/main/webapp/WEB-INF/controllers.xml"};
+    }
+}
