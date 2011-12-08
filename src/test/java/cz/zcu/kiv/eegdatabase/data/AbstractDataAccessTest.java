05f05ecb42d2ce9cb9404f068cb5834d3e5e0582
jnovotny <jnovotny@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2011-12-08 08:57:09 +0000

    AbstractDataAccessTest (root of hibernate tests) now changes the default parser to Xerces in constructor (change needs to be done before Spring & Hibernate is initialized) via System properties. ->No configuration change is needed in IDE or maven to run the tests. Cca 25 test methods (of 59) are now succeeding.
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1110 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/test/java/cz/zcu/kiv/eegdatabase/data/AbstractDataAccessTest.java
+++ src/test/java/cz/zcu/kiv/eegdatabase/data/AbstractDataAccessTest.java
@@ -14,8 +14,23 @@ public class AbstractDataAccessTest extends AbstractTransactionalDataSourceSprin
     private SessionFactory sessionFactory;
 
     public AbstractDataAccessTest() {
+        changeParserImplementationToXerces();//Important!
         setDependencyCheck(false);
         setAutowireMode(AUTOWIRE_BY_NAME);
+    }
+
+    /**
+     * If not changed, oracle parser would try to parse hibernate configurations
+     * and fail with the following error:
+     * ERROR ErrorLogger - Error parsing XML (31) : http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd<Line 31, Column 2>:
+     * XML-20068: (Fatal Error) content model is not deterministic
+     * org.hibernate.InvalidMappingException: Unable to read XML
+     * Setting SAXParserFactory and DocumentBuilderFactory will change the parser
+     * to xerces, enabling Hibernate-based tests
+     */
+    private void changeParserImplementationToXerces() {
+        System.setProperty("javax.xml.parsers.SAXParserFactory","org.apache.xerces.jaxp.SAXParserFactoryImpl");
+        System.setProperty("javax.xml.parsers.DocumentBuilderFactory","org.apache.xerces.jaxp.DocumentBuilderFactoryImpl");
     }
 
     protected String[] getConfigLocations() {
