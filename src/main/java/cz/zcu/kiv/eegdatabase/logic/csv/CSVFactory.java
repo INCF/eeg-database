329a338b50b3db0d087ad917c44dc20421ae745a
bruhap <bruhap@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2011-12-13 13:42:59 +0000

    Generating CSV file (desription scenario) for NIF
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1119 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/main/java/cz/zcu/kiv/eegdatabase/logic/csv/CSVFactory.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/logic/csv/CSVFactory.java
@@ -1,9 +1,14 @@
 package cz.zcu.kiv.eegdatabase.logic.csv;
 
+import cz.zcu.kiv.eegdatabase.data.dao.GenericDao;
+import cz.zcu.kiv.eegdatabase.data.dao.ScenarioDao;
+import cz.zcu.kiv.eegdatabase.data.pojo.Scenario;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.*;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,19 +19,33 @@ import java.io.*;
  */
 public class CSVFactory {
     private Log log = LogFactory.getLog(getClass());
+    @Autowired
+    private ScenarioDao scenarioDao;
+
+    private String domain;
 
     public OutputStream generateCsvFile() throws IOException {
 
         log.debug("Creating output stream");
         OutputStream out = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(out);
-        printStream.println("test;test");
-        printStream.println("test;test");
+        List<Scenario> scenarioList = scenarioDao.getAllRecords();
 
+        printStream.println(CSVUtils.SCENARIO_TITLE+CSVUtils.SEMICOLON+CSVUtils.SCENARIO_LENGTH+CSVUtils.SEMICOLON+CSVUtils.SCENARIO_DESCRIPTION+CSVUtils.SEMICOLON+CSVUtils.SCENARIO_DETAILS);
+
+        for (int i = 0; i < scenarioList.size(); i++) {
+            printStream.println(scenarioList.get(i).getScenarioName() + CSVUtils.SEMICOLON + scenarioList.get(i).getScenarioLength() + CSVUtils.SEMICOLON + scenarioList.get(i).getDescription()+CSVUtils.SEMICOLON+domain+CSVUtils.SCENARIO_URL+scenarioList.get(i).getScenarioId());
+        }
         printStream.close();
 
         return out;
+    }
 
+    public String getDomain() {
+        return domain;
+    }
 
+    public void setDomain(String domain) {
+        this.domain = domain;
     }
 }
---
+++ src/main/java/cz/zcu/kiv/eegdatabase/logic/csv/CSVUtils.java
@@ -0,0 +1,21 @@
+package cz.zcu.kiv.eegdatabase.logic.csv;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pbruha
+ * Date: 13.12.11
+ * Time: 13:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class CSVUtils {
+    public static final String SCENARIO_URL = "scenario.url=/scenarios/detail.html?scenarioId=";
+    public static final String EXPERIMENT_URL = "experiment.url=/experiments/detail.html?experimentId=";
+    public static final String SCENARIO_TITLE = "Scenario title";
+    public static final String SCENARIO_LENGTH = "Length";
+    public static final String SCENARIO_DESCRIPTION = "Description";
+    public static final String SCENARIO_DETAILS ="Scenario details";
+    public static final String SEMICOLON =";";
+
+
+
+}
