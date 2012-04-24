722464df6a540585fc3002174b05bef2d3019305
stebjan <stebjan@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2012-04-24 13:01:00 +0000

    First pack of DAO objects.
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1246 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/main/java/cz/zcu/kiv/eegdatabase/data/dao/EducationLevelDao.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/data/dao/EducationLevelDao.java
@@ -1,18 +1,15 @@
 package cz.zcu.kiv.eegdatabase.data.dao;
 
 import cz.zcu.kiv.eegdatabase.data.pojo.EducationLevel;
-import cz.zcu.kiv.eegdatabase.data.pojo.Person;
-import cz.zcu.kiv.eegdatabase.logic.controller.search.SearchRequest;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Dao for EducationLevel
  *
  * @author Jiri Novotny
  */
-public interface EducationLevelDao extends GenericDao<EducationLevel, Integer> {
+public interface EducationLevelDao extends GenericListDaoWithDefault<EducationLevel> {
 
     public List<EducationLevel> getEducationLevels(String title);
 
