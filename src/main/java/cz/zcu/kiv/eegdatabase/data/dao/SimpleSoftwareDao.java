f17610fe2ef2de25923aee0798454173e23fde3f
stebjan <stebjan@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2012-04-25 12:20:34 +0000

    All remaining DAO objects are added.
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1247 1a751040-f3c7-4aaa-98da-1e85aacc1d30

---
+++ src/main/java/cz/zcu/kiv/eegdatabase/data/dao/SimpleSoftwareDao.java
@@ -0,0 +1,87 @@
+package cz.zcu.kiv.eegdatabase.data.dao;
+
+import cz.zcu.kiv.eegdatabase.data.pojo.ResearchGroup;
+import cz.zcu.kiv.eegdatabase.data.pojo.Software;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stebjan
+ * Date: 25.4.12
+ * Time: 14:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class SimpleSoftwareDao extends SimpleGenericDao<Software, Integer>
+        implements GenericListDaoWithDefault<Software>{
+
+    public SimpleSoftwareDao() {
+        super(Software.class);
+    }
+
+    @Override
+    public void createGroupRel(Software persistent, ResearchGroup researchGroup) {
+        persistent.getResearchGroups().add(researchGroup);
+        researchGroup.getSoftwares().add(persistent);
+    }
+
+    @Override
+    public List<Software> getItemsForList() {
+        String hqlQuery = "from Software st order by st.description";
+        return getHibernateTemplate().find(hqlQuery);
+    }
+
+    @Override
+    public List<Software> getRecordsByGroup(int groupId) {
+        String hqlQuery = "from Software st inner join fetch st.researchGroups as rg where rg.researchGroupId="+groupId+" ";
+        return getHibernateTemplate().find(hqlQuery);
+
+    }
+
+    @Override
+    public boolean canDelete(int id) {
+       String hqlQuery = "select st.experiments from Software st where st.softwareId = :id";
+        String[] names = {"id"};
+        Object[] values = {id};
+        List<Software> list = getHibernateTemplate().findByNamedParam(hqlQuery, names, values);
+        return (list.size() == 0);
+    }
+
+    @Override
+    public boolean hasGroupRel(int id) {
+        String hqlQuery = "from Software st where st.softwareId = :id";
+        String[] names = {"id"};
+        Object[] values = {id};
+        List<Software> list = getHibernateTemplate().findByNamedParam(hqlQuery, names, values);
+        return list.get(0).getResearchGroups().size() > 0;
+    }
+
+    @Override
+    public void deleteGroupRel(Software persistent, ResearchGroup researchGroup) {
+        persistent.getResearchGroups().remove(researchGroup);
+        researchGroup.getSoftwares().remove(persistent);
+    }
+
+    @Override
+    public void createDefaultRecord(Software persistent) {
+        persistent.setDefaultNumber(1);
+        create(persistent);
+    }
+
+    @Override
+    public List<Software> getDefaultRecords() {
+        String hqlQuery = "from Software st where st.defaultNumber=1";
+        return getHibernateTemplate().find(hqlQuery);
+    }
+
+    @Override
+    public boolean isDefault(int id) {
+       String hqlQuery = "select st.defaultNumber from Software st where st.softwareId="+id+" ";
+        List<Integer> list = getHibernateTemplate().find(hqlQuery);
+        if(list.isEmpty()){
+            return false;
+        }
+        return (list.get(0)==1);
+
+    }
+}
