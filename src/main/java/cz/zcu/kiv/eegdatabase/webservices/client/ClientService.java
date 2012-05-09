754700925e03b3b580a25aec8a7dfa3514ccc0c7
fliska <fliska@1a751040-f3c7-4aaa-98da-1e85aacc1d30>
2012-05-09 22:29:04 +0000

    ClientService - added getAllPeople()
    git-svn-id: https://svn.origo.ethz.ch/eeg-database/trunk@1275 1a751040-f3c7-4aaa-98da-1e85aacc1d30

--- src/main/java/cz/zcu/kiv/eegdatabase/webservices/client/ClientService.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/webservices/client/ClientService.java
@@ -1,13 +1,15 @@
 package cz.zcu.kiv.eegdatabase.webservices.client;
 
+import cz.zcu.kiv.eegdatabase.webservices.dataDownload.wrappers.PersonInfo;
+
 import javax.jws.WebService;
+import java.util.List;
+
 /**
  * @author František Liška
  */
 @WebService
 public interface ClientService {
-    String sayHi(String text);
-
     boolean isServiceAvailable();
-
+    public List<PersonInfo> getAllPeople();
 }
