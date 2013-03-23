1fe99542a06fbdbfff57b501977a8b06d85c71fc
Petr Miko <miko.petr@gmail.com>
2013-03-23 23:38:52 +0100

    REST: registration with password, mail included
    Confirmation mail includes password, which can be used after
    confirmation.

--- src/main/java/cz/zcu/kiv/eegdatabase/webservices/rest/user/UserService.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/webservices/rest/user/UserService.java
@@ -1,7 +1,7 @@
 package cz.zcu.kiv.eegdatabase.webservices.rest.user;
 
-import cz.zcu.kiv.eegdatabase.logic.controller.person.AddPersonCommand;
 import cz.zcu.kiv.eegdatabase.webservices.rest.common.exception.RestServiceException;
+import cz.zcu.kiv.eegdatabase.webservices.rest.user.wrappers.PersonData;
 import cz.zcu.kiv.eegdatabase.webservices.rest.user.wrappers.UserInfo;
 
 import java.util.Locale;
@@ -17,15 +17,16 @@ public interface UserService {
      * Creates new user inside eeg base.
      *
      * @param registrationPath URL of registration confirmation page
-     * @param cmd new person/user information
-     * @param locale used locale
+     * @param personData       new person/user information
+     * @param locale           used locale
      * @return basic information about created person/user
      * @throws RestServiceException error during person/user creation process
      */
-    public UserInfo create(String registrationPath, AddPersonCommand cmd, Locale locale) throws RestServiceException;
+    public UserInfo create(String registrationPath, PersonData personData, Locale locale) throws RestServiceException;
 
     /**
      * Method for verifying user's credentials.
+     *
      * @return basic information about accessing person/user
      */
     public UserInfo login();
--- src/main/java/cz/zcu/kiv/eegdatabase/webservices/rest/user/UserServiceImpl.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/webservices/rest/user/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package cz.zcu.kiv.eegdatabase.webservices.rest.user;
 
+import cz.zcu.kiv.eegdatabase.data.dao.EducationLevelDao;
 import cz.zcu.kiv.eegdatabase.data.dao.PersonDao;
+import cz.zcu.kiv.eegdatabase.data.pojo.EducationLevel;
 import cz.zcu.kiv.eegdatabase.data.pojo.Person;
-import cz.zcu.kiv.eegdatabase.logic.controller.person.AddPersonCommand;
+import cz.zcu.kiv.eegdatabase.logic.Util;
+import cz.zcu.kiv.eegdatabase.logic.util.ControllerUtils;
+import cz.zcu.kiv.eegdatabase.webservices.client.wrappers.EducationLevelInfo;
 import cz.zcu.kiv.eegdatabase.webservices.rest.common.exception.RestServiceException;
+import cz.zcu.kiv.eegdatabase.webservices.rest.user.wrappers.PersonData;
 import cz.zcu.kiv.eegdatabase.webservices.rest.user.wrappers.UserInfo;
 import cz.zcu.kiv.eegdatabase.wui.core.person.PersonService;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +20,15 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.sql.Timestamp;
 import java.text.ParseException;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -31,6 +38,8 @@ import java.util.Locale;
  */
 @Service
 public class UserServiceImpl implements UserService {
+
+    private String DEFAULT_EDUCATION_LEVEL = "NotKnown";
 
     private final static Log log = LogFactory.getLog(UserServiceImpl.class);
     @Qualifier("mailSender")
@@ -42,24 +51,47 @@ public class UserServiceImpl implements UserService {
     @Qualifier("mailMessage")
     @Autowired
     private SimpleMailMessage mailMessage;
-    
-    @Deprecated()
     @Autowired
     @Qualifier("personDao")
     private PersonDao personDao;
-    
-    @Qualifier("personService")
+    @Qualifier("educationLevelDao")
     @Autowired
-    private PersonService personService;
+    private EducationLevelDao educationLevelDao;
 
     /**
      * @{@inheritDoc}
      */
+    @Transactional
     @Override
-    public UserInfo create(String registrationPath, AddPersonCommand cmd, Locale locale) throws RestServiceException {
+    public UserInfo create(String registrationPath, PersonData personData, Locale locale) throws RestServiceException {
         try {
-            Person person = personService.createPerson(cmd);
-            sendRegistrationConfirmMail(registrationPath, person, locale);
+            Person person = new Person();
+            person.setGivenname(personData.getName());
+            person.setSurname(personData.getSurname());
+            person.setPassword(personData.getPassword() == null ? ControllerUtils.getRandomPassword() : personData.getPassword());
+            String plainPassword = person.getPassword();
+
+            person.setRegistrationDate(new Timestamp(System.currentTimeMillis()));
+            person.setGender(personData.getGender().charAt(0));
+            person.setLaterality(personData.getLeftHanded().charAt(0));
+
+            person.setDateOfBirth(new Timestamp(ControllerUtils.getDateFormat().parse(personData.getBirthday()).getTime()));
+            person.setPhoneNumber(personData.getPhone());
+            person.setNote(personData.getNotes());
+
+            //dummy default education level
+            List<EducationLevel> def = educationLevelDao.getEducationLevels(DEFAULT_EDUCATION_LEVEL);
+            person.setEducationLevel(def.isEmpty() ? null : def.get(0));
+            //default role
+            person.setAuthority(Util.ROLE_READER);
+
+            // security specifics
+            person.setUsername(personData.getEmail());
+            person.setAuthenticationHash(ControllerUtils.getMD5String(personData.getEmail()));
+            person.setPassword(new BCryptPasswordEncoder().encode(plainPassword));
+
+            personDao.create(person);
+            sendRegistrationConfirmMail(registrationPath, plainPassword,person, locale);
             return new UserInfo(person.getGivenname(), person.getSurname(), person.getAuthority());
         } catch (ParseException e) {
             log.error(e.getMessage(), e);
@@ -76,19 +108,21 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional(readOnly = true)
     public UserInfo login() {
-        Person user = personService.getLoggedPerson();
+        Person user = personDao.getLoggedPerson();
         return new UserInfo(user.getGivenname(), user.getSurname(), user.getAuthority());
     }
 
     /**
      * Method for sending registration confirmation email.
+     *
      * @param registrationPath path to registration confirmation page
-     * @param user created user object
-     * @param locale locale
-     * @throws MailException error during sending mail
+     * @param plainPassword    password in plain text
+     * @param user             created user object
+     * @param locale           locale
+     * @throws MailException      error during sending mail
      * @throws MessagingException error during sending mail
      */
-    private void sendRegistrationConfirmMail(String registrationPath, Person user, Locale locale) throws MailException, MessagingException {
+    private void sendRegistrationConfirmMail(String registrationPath, String plainPassword, Person user, Locale locale) throws MailException, MessagingException {
         log.debug("Creating email content");
         StringBuilder sb = new StringBuilder();
         String login = "<b>" + user.getUsername() + "</b>";
@@ -99,6 +133,9 @@ public class UserServiceImpl implements UserService {
         sb.append("</h4>");
         sb.append("<p>");
         sb.append(messageSource.getMessage("registration.email.body.yourLogin", new String[]{login}, locale));
+        sb.append("</p>");
+        sb.append("<p>");
+        sb.append(messageSource.getMessage("registration.email.body.yourPassword", new String[]{plainPassword}, locale));
         sb.append("</p>");
         sb.append("<p>");
         sb.append(messageSource.getMessage("registration.email.body.clickToRegister", null, locale));
