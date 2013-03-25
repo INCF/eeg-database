1f0640fc4585acedc69f98a03c028eca8fab7780
rinkesj <rinkesj@students.zcu.cz>
2013-03-25 15:13:17 +0100

    fixed multiple loading data from database in lists. fixed missing dependency in project. stop comment lucene library or remove it from source code.

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/lists/components/ListModelWithResearchGroupCriteria.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/lists/components/ListModelWithResearchGroupCriteria.java
@@ -2,21 +2,24 @@ package cz.zcu.kiv.eegdatabase.wui.ui.lists.components;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.util.ListModel;
 
 import cz.zcu.kiv.eegdatabase.data.pojo.ResearchGroup;
 
-public abstract class ListModelWithResearchGroupCriteria<T> extends ListModel<T> {
+public abstract class ListModelWithResearchGroupCriteria<T> extends LoadableDetachableModel<List<T>> {
 
     private static final long serialVersionUID = -8410077884285556462L;
+    protected Log log = LogFactory.getLog(getClass());
 
     private Model<ResearchGroup> criteriaModel = new Model<ResearchGroup>(null);
 
     protected abstract List<T> loadList(ResearchGroup group);
-
+    
     @Override
-    public List<T> getObject() {
+    protected List<T> load() {
         return loadList(criteriaModel.getObject());
     }
 
