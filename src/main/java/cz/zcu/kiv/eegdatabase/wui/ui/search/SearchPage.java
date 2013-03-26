e120fac5f371e9a29929c901575e8d84faa23f14
Jan Koren <hanysz@centrum.cz>
2013-03-26 12:37:00 +0100

    Full text service update
    - Searching documents by the query string
    - expansion of results (not used yet)
    - minor refactoring
    - added search utility class storing constants

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/search/SearchPage.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/search/SearchPage.java
@@ -1,24 +1,20 @@
 package cz.zcu.kiv.eegdatabase.wui.ui.search;
 
+import cz.zcu.kiv.eegdatabase.logic.controller.searchsolr.FullTextResult;
 import cz.zcu.kiv.eegdatabase.logic.controller.searchsolr.FulltextSearchService;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.SolrInputDocument;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
-import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-
 import cz.zcu.kiv.eegdatabase.wui.components.page.MenuPage;
 import cz.zcu.kiv.eegdatabase.wui.components.utils.ResourceUtils;
-import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteTextField;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.util.string.Strings;
+import org.apache.wicket.util.string.StringValue;
 
-import java.util.*;
+import java.util.List;
 
 @AuthorizeInstantiation(value = { "ROLE_USER", "ROLE_EXPERIMENTER", "ROLE_ADMIN" })
 public class SearchPage extends MenuPage {
@@ -28,52 +24,41 @@ public class SearchPage extends MenuPage {
     @SpringBean
     private FulltextSearchService searchService;
 
-    public SearchPage() throws SolrServerException {
+    private boolean isSearchStringEmpty = false;
+
+    public SearchPage(PageParameters params) {
         
         setPageTitle(ResourceUtils.getModel("title.page.search"));
 
-        //add(new Label("searchResult", String.valueOf(searchService.getAllDocuments().size())));
+        StringValue searchString = params.get("searchString");
+        if (searchString.isEmpty() || searchString.isNull()) {
+            isSearchStringEmpty = true;
+        }
+        IModel<List<FullTextResult>> fullTextResults = new ListModel<FullTextResult>();
+        fullTextResults.setObject(searchService.getResultsForQuery(searchString.toString()));
 
-        Form<Void> searchForm = new Form<Void>("searchForm");
-        add(searchForm);
+        SearchPanel searchPanel = new SearchPanel("searchPanel");
+        SearchResultPanel searchResultPanel = new SearchResultPanel("resultPanel", fullTextResults) {
 
-        final AutoCompleteTextField<String> searchField = new AutoCompleteTextField<String>(
-                "autocomplete", new Model<String>("")) {
             @Override
-            protected Iterator<String> getChoices(String input) {
-                return getAutoCompleteChoices(input);
+            public boolean isVisible() {
+                return !isSearchStringEmpty;
             }
         };
-        searchForm.add(searchField);
+        add(searchPanel);
+        add(searchResultPanel);
 
-        /*
-        searchField.add(new AjaxFormSubmitBehavior(searchForm, "onchange") {
-            @Override
-            protected void onSubmit(AjaxRequestTarget target) {
-                super.onSubmit(target);
-            }
-
-            @Override
-            protected void onError(AjaxRequestTarget target) {
-                super.onError(target);
-            }
-        });
-        */
-
+        add(new FeedbackPanel("feedback"));
     }
 
-    private Iterator<String> getAutoCompleteChoices(String input) {
-        if(Strings.isEmpty(input)) {
-            List<String> emptyList = Collections.emptyList();
-            return emptyList.iterator();
-        }
+    private LoadableDetachableModel<List<FullTextResult>> createModelForFulltext(final TextField<String> textField) {
+        return new LoadableDetachableModel<List<FullTextResult>>() {
+            @Override
+            protected List<FullTextResult> load() {
+                List<FullTextResult> queryResults = searchService.getResultsForQuery(textField.getModelObject());
 
-        Set<String> autocompletePhrases = new HashSet<String>(10);
-        try {
-            autocompletePhrases = searchService.getTextToAutocomplete(input);
-        } catch (SolrServerException e) {
-            e.printStackTrace();
-        }
-        return autocompletePhrases.iterator();
+                return queryResults;
+            }
+        };
     }
 }
