2a068ed89e2eb15b3588d6a52174da8b58e38588
Jan Koren <hanysz@centrum.cz>
2013-04-10 03:29:28 +0200

    Major indexing changes and updates - implementation of a different POJO indexing strategy. See JavaDoc and code comments for more details. - added autocomplete indexer that indexes searched phrases and takes their search frequencies into account - the most searched ones are then displayed first. - both POJO and LinkedIn full indexing is set to run periodically (configured by the Spring Task Scheduler).

---
+++ src/main/java/cz/zcu/kiv/eegdatabase/data/indexing/IndexingServiceImpl.java
@@ -0,0 +1,148 @@
+package cz.zcu.kiv.eegdatabase.data.indexing;
+
+import cz.zcu.kiv.eegdatabase.data.dao.GenericDao;
+import cz.zcu.kiv.eegdatabase.logic.controller.social.LinkedInManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.social.linkedin.api.Post;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+/**
+ * Implementation of the indexing service.
+ * User: Jan Koren
+ * Date: 12.3.13
+ */
+@Service
+public class IndexingServiceImpl implements IndexingService, ApplicationContextAware {
+
+    private Log log = LogFactory.getLog(getClass());
+
+    @Autowired
+    private Indexer indexer;
+    @Autowired
+    private Indexer<Post> linkedInIndexer;
+    @Autowired
+    private Indexer<String> autocompleteIndexer;
+
+    // if required, the tests would fail because the bean is not wired in the test context
+    @Autowired(required = false)
+    private LinkedInManager linkedin;
+
+    ApplicationContext applicationContext = null;
+
+    private static final int LINKEDIN_POSTS_STEP = 50;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * Performs indexing of the whole database.
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException
+     * @throws IOException
+     * @throws SolrServerException
+     */
+    @Async
+    public void indexDatabase() throws IllegalAccessException, SolrServerException, IOException, NoSuchMethodException, InstantiationException {
+
+        // get required dao beans
+        Set<Class<? extends GenericDao>> daoClasses = IndexingUtils.getDaosForIndexing();
+
+        for (Class<? extends GenericDao> daoClass : daoClasses) {
+            log.debug("class: " + daoClass.getName());
+
+            List daoRecords = applicationContext.getBean(daoClass).getAllRecordsFull();
+
+            indexer.indexAll(daoRecords);
+        }
+    }
+
+    /**
+     * Does indexing of all LinkedIn posts.
+     * @throws IllegalAccessException
+     * @throws SolrServerException
+     * @throws IOException
+     */
+    @Async
+    public void indexLinkedIn() throws IllegalAccessException, SolrServerException, IOException {
+
+        int startIndex = 0;
+        List<Post> posts = new ArrayList<Post>();
+        List<Post> receivedPosts = linkedin.getGroupPostsWithMoreInfo(LINKEDIN_POSTS_STEP, startIndex);
+        // additional iterations are necessary due to the limit of 50 returned posts at maximum (LinkedIn API restriciton)
+        while (receivedPosts != null) {
+            posts.addAll(receivedPosts);
+            startIndex += LINKEDIN_POSTS_STEP;
+            receivedPosts = linkedin.getGroupPostsWithMoreInfo(LINKEDIN_POSTS_STEP, startIndex);
+        }
+
+        linkedInIndexer.indexAll(posts);
+    }
+
+    @Async
+    public void addToAutocomplete(String phrase) {
+        try {
+            autocompleteIndexer.index(phrase);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Does indexing of specific LinkedIn posts.
+     * @param posts Posts that are to be indexed.
+     * @throws IllegalAccessException
+     * @throws SolrServerException
+     * @throws IOException
+     */
+    public void indexLinkedIn(List<Post> posts) throws IllegalAccessException, SolrServerException, IOException {
+        linkedInIndexer.indexAll(posts);
+    }
+
+    /**
+     * Does indexing of both database and LinkedIn posts.
+     * @throws IllegalAccessException
+     * @throws SolrServerException
+     * @throws InstantiationException
+     * @throws IOException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws ClassNotFoundException
+     */
+    @Scheduled(fixedDelay = 5*60*1000)
+    public void indexAll() {
+        try {
+            indexDatabase();
+            indexLinkedIn();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/cz/zcu/kiv/eegdatabase/data/indexing/PojoIndexer.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/data/indexing/PojoIndexer.java
@@ -1,36 +1,107 @@
 package cz.zcu.kiv.eegdatabase.data.indexing;
 
+import cz.zcu.kiv.eegdatabase.data.annotation.Indexed;
 import cz.zcu.kiv.eegdatabase.data.annotation.SolrField;
 import cz.zcu.kiv.eegdatabase.data.annotation.SolrId;
+import cz.zcu.kiv.eegdatabase.logic.controller.searchsolr.FullTextSearchUtils;
+import javassist.util.proxy.MethodFilter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.jaxrs.ext.search.SearchUtils;
+import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.SolrInputField;
+import org.hibernate.Hibernate;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.reflections.Reflections;
+import org.springframework.util.ReflectionUtils;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Iterator;
+import java.lang.reflect.*;
+import java.util.*;
 
 /**
- * Created with IntelliJ IDEA.
+ * Indexer of POJO classes.
+ *
  * User: Jan Koren
  * Date: 19.2.13
- * Time: 0:30
- * To change this template use File | Settings | File Templates.
  */
-public class PojoIndexer<T> extends Indexer<T> {
+public class PojoIndexer extends Indexer {
 
 
     protected Log log = LogFactory.getLog(getClass());
+
+    // levels of indexing traversal
+    private final int LEVEL_PARENT = 0;
+    private final int LEVEL_CHILD = 1;
 
     public PojoIndexer() {
     }
 
     @Override
-    public SolrInputDocument prepareForIndexing(T instance) throws IOException, SolrServerException, IllegalAccessException {
-        SolrInputDocument document = getDocumentFromAnnotatedFields(instance);
-        document = createDocumentId(instance, document);
+    public SolrInputDocument prepareForIndexing(Object instance) throws IOException, SolrServerException, IllegalAccessException {
+
+        Class clazz = instance.getClass();
+        // first find out whether the class shall be indexed
+        if(!clazz.isAnnotationPresent(Indexed.class)) {
+            return null;
+        }
+
+        String className = instance.getClass().getName();
+        Field[] fields = clazz.getDeclaredFields();
+
+        // pak zjisteni, jestli nektery jeji field ma anotaci @SolrId
+        int id = getId(fields, instance);
+        // a z id vytvorit nasledujici fieldy Solr dokumentu
+        SolrInputDocument document = new SolrInputDocument();
+        document.addField(IndexField.UUID.getValue(), className + id);
+        document.addField(IndexField.ID.getValue(), id);
+        document.addField(IndexField.CLASS.getValue(), FullTextSearchUtils.getDocumentType(clazz));
+
+        // pak vytazeni hodnot fieldu, ktere maji anotaci @SolrField
+        //SolrInputDocument document = getDocumentFromAnnotatedFields(instance);
+        Map<String, Object> documentFields = null;
+        try {
+            documentFields = getDocumentFromAnnotatedFields(instance, LEVEL_PARENT);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        for(String key : documentFields.keySet()) {
+            document.addField(key, documentFields.get(key));
+        }
+
+
+        // pote projit vsechny kolekce, pro kazdy objekt kolekce najit @SolrField anotace u jeho fieldu
+        // a jeho @SolrId pro sparovani polozek v multivalued listu rodicovskeho dokumentu
+
+
+        //document = createDocumentId(instance, document);
         return document;
+    }
+
+    private int getId(Field[] fields, Object instance) {
+        for (Field field : fields) {
+            // add uuid, class name and id of the instance to the solr index
+            if (field.isAnnotationPresent(SolrId.class)) {
+                field.setAccessible(true); // necessary since the id field is private
+                int id = 0;
+                try {
+                    id = (Integer) field.get(instance);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+                log.debug("ID: " + id);
+                return id;
+            }
+        }
+        // class doesn't contain the @SolrId annotation, throw an exception
+        throw new RuntimeException("None of the fields contains the @SolrId annotation.");
     }
 
     /**
@@ -40,7 +111,7 @@ public class PojoIndexer<T> extends Indexer<T> {
      * @throws IOException
      * @throws SolrServerException
      */
-    public void unindex(T instance) throws IOException, SolrServerException {
+    public void unindex(Object instance) throws IOException, SolrServerException {
 
         Field[] fields = instance.getClass().getDeclaredFields();
         String className = instance.getClass().getName();
@@ -68,8 +139,22 @@ public class PojoIndexer<T> extends Indexer<T> {
         solrServer.commit();
     }
 
-    private SolrInputDocument getDocumentFromAnnotatedFields(T instance) throws IllegalAccessException {
-        SolrInputDocument document = new SolrInputDocument();
+    /**
+     * Creates a document for indexing. The document consists of values of fields having indexing annotations.
+     * Depending on the annotation type, fields are either put to the document or traversed recursively to pick
+     * property values of nested objects.
+     * @param instance Parent object to be indexed,
+     * @param level Current level of traversal. Can be either parent or child level. In case of the parent level, the
+     *              algorithm can continue indexing the child elements located in the child level. In both levels
+     *              indexing of String and primitive types is allowed.
+     * @return Field-value pairs representing a document to be indexed.
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     */
+    private Map<String, Object> getDocumentFromAnnotatedFields(Object instance, int level) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Map<String, Object> solrFields = new HashMap<String, Object>();
+        //SolrInputDocument document = new SolrInputDocument();
         Field[] fields = instance.getClass().getDeclaredFields();
         for (Field field : fields) {
             // check presence of the SolrField annotation for each field
@@ -77,46 +162,84 @@ public class PojoIndexer<T> extends Indexer<T> {
                 field.setAccessible(true); // necessary since all fields are private
                 Object fieldValue = field.get(instance); // actual value of the annotated field
                 log.debug(instance.getClass().getName());
-
                 // null values are skipped
                 if (fieldValue == null) {
                     continue;
                 }
+                String fieldName = field.getAnnotation(SolrField.class).name().getValue();
 
-                log.info("Indexing - field: "
-                        + field.getAnnotation(SolrField.class).name().getValue()
+                if(level == LEVEL_CHILD) {
+                    fieldName = "child_" + fieldName;
+                }
+
+                log.debug("Indexing - field: " + fieldName
                         + " value: " + fieldValue.toString());
 
-                document.addField(field.getAnnotation(SolrField.class).name().getValue(), fieldValue);
-            }
-        }
-
-        return document;
-    }
-
-    private SolrInputDocument createDocumentId(T instance, SolrInputDocument document) {
-        Field[] fields = instance.getClass().getDeclaredFields();
-        String className = instance.getClass().getName();
-        for (Field field : fields) {
-            // add uuid, class name and id of the instance to the solr index
-            if (field.isAnnotationPresent(SolrId.class)) {
-                field.setAccessible(true); // necessary since the id field is private
-                int id = 0;
-                try {
-                    id = (Integer) field.get(instance);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
+                // POJO has more fields having the same annotation parameter
+                if(solrFields.containsKey(fieldName)) {
+                    solrFields.put(fieldName, solrFields.get(fieldName) + " " + fieldValue);
                 }
-                log.debug("ID: " + id);
-                document.addField(IndexField.UUID.getValue(), className + id);
-                document.addField(IndexField.ID.getValue(), id);
-                document.addField(IndexField.CLASS.getValue(), className);
-                break;
+                // the annotated value is being read for the first time
+                else {
+                    solrFields.put(fieldName, fieldValue);
+                }
+            }
+            // traverse collections and objects of the parent object
+            else if (level == LEVEL_PARENT) {
+                // check if it is a collection
+                if (field.isAnnotationPresent(Indexed.class) && Collection.class.isAssignableFrom(field.getType())) {
+                    field.setAccessible(true); // necessary since all fields are private
+                    Object collectionInstance = field.get(instance);
+                    if (collectionInstance == null) {
+                        continue;
+                    }
+                    // convert the collection to an array to enable transparent manipulation independent
+                    // on the specific collection subclass
+                    Method objectToArray = field.getType().getMethod("toArray");
+                    Object array = objectToArray.invoke(collectionInstance);
+                    // get array size
+                    int length = Array.getLength(array);
+
+                    Map<String, List<Object>> solrFieldsChildren = new HashMap<String, List<Object>>();
+                    for(int i = 0; i < length; i++) {
+                        Object element = Array.get(array, i);
+                        // scan only the @SolrField-annotated fields of the child objects
+                        Map<String, Object> solrFieldsChild = getDocumentFromAnnotatedFields(element, LEVEL_CHILD);
+                        // add all field-value pairs of each object of the collection to the map of the whole collection
+                        for(Map.Entry<String, Object> entry : solrFieldsChild.entrySet()) {
+
+                            if(solrFieldsChildren.containsKey(entry.getKey())) {
+                                List<Object> list = solrFieldsChildren.get(entry.getKey());
+                                list.add(entry.getValue());
+                                solrFieldsChildren.put(entry.getKey(), list);
+                            }
+                            else {
+                                List<Object> list = new ArrayList<Object>();
+                                list.add(entry.getValue());
+                                solrFieldsChildren.put(entry.getKey(), list);
+                            }
+                        }
+                    }
+                    // add the index values of the children from the collection to the parent fields
+                    solrFields.putAll(solrFieldsChildren);
+                }
+                // check if it's an object
+                else if (field.isAnnotationPresent(Indexed.class) && field.getType().isInstance(Object.class)) {
+                    // scan only the @SolrField-annotated fields of the child objects
+                    Map<String, Object> solrFieldsChild = getDocumentFromAnnotatedFields(field.get(instance), LEVEL_CHILD);
+                    solrFields.putAll(solrFieldsChild);
+                }
             }
         }
-        return document;
+
+        //return document;
+        return solrFields;
     }
 
+    /**
+     * Writes field-value pairs for a given solr document.
+     * @param document The input solr document.
+     */
     public void getDocumentFieldValuePairs(SolrInputDocument document) {
         Iterator<String> nameIterator = document.getFieldNames().iterator();
         while (nameIterator.hasNext()) {
