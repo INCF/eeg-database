14d357c757e543a5100211d8d9fcf20574396e5c
Jakub Balhar <balhar.jakub@gmail.com>
2013-04-16 14:22:26 +0200

    Funkční AddGroup Formulář

---
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/forms/GroupForm.java
@@ -0,0 +1,94 @@
+package cz.zcu.kiv.eegdatabase.wui.ui.experiments.forms;
+
+import cz.zcu.kiv.eegdatabase.data.pojo.ResearchGroup;
+import cz.zcu.kiv.eegdatabase.wui.app.session.EEGDataBaseSession;
+import cz.zcu.kiv.eegdatabase.wui.core.group.ResearchGroupFacade;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormValidatingBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.time.Duration;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jakub Balhar
+ * Date: 16.4.13
+ * Time: 13:53
+ */
+public class GroupForm extends Form<ResearchGroup> {
+    @SpringBean
+    ResearchGroupFacade researchGroupFacade;
+
+
+    public GroupForm(String id, final ModalWindow window) {
+        super(id, new CompoundPropertyModel<ResearchGroup>(new ResearchGroup()));
+
+        final FeedbackPanel feedback = new FeedbackPanel("feedback");
+        feedback.setOutputMarkupId(true);
+        add(feedback);
+        add(new Label("addGroupHeader", "Create group"));
+
+        add(
+                new RequiredTextField<String>("title").
+                        add(new TitleExistsValidator())
+        );
+        add(new TextArea<String>("description").setRequired(true));
+
+        add(
+                new AjaxButton("submitForm", this) {
+                    @Override
+                    protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                        ResearchGroup researchGroup = (ResearchGroup) form.getModelObject();
+
+                        validate();
+                        target.add(feedback);
+
+                        if(!hasError()){
+                            researchGroup.setPerson(EEGDataBaseSession.get().getLoggedUser());
+                            researchGroupFacade.create(researchGroup);
+                            window.close(target);
+                        }
+                    }
+                }.add(new AjaxEventBehavior("onclick") {
+                    @Override
+                    protected void onEvent(AjaxRequestTarget target) {
+                    }
+                })
+        );
+
+        add(
+                new AjaxButton("closeForm", this) {}.
+                        add(new AjaxEventBehavior("onclick") {
+                            @Override
+                            protected void onEvent(AjaxRequestTarget target) {
+                                window.close(target);
+                            }
+                        })
+        );
+
+        setOutputMarkupId(true);
+        AjaxFormValidatingBehavior.addToAllFormComponents(this, "focus", Duration.ONE_SECOND);
+    }
+
+    private class TitleExistsValidator implements IValidator<String> {
+
+        @Override
+        public void validate(IValidatable<String> validatable) {
+            final String title = validatable.getValue();
+
+            if(researchGroupFacade.existsGroup(title)){
+                error("Research group with given name already exists.");
+            }
+        }
+    }
+}
--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/modals/AddGroupPage.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/modals/AddGroupPage.java
@@ -1,19 +1,12 @@
 package cz.zcu.kiv.eegdatabase.wui.ui.experiments.modals;
 
+import cz.zcu.kiv.eegdatabase.wui.ui.experiments.forms.GroupForm;
 import org.apache.wicket.PageReference;
-import org.apache.wicket.ajax.AjaxEventBehavior;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
-import org.apache.wicket.extensions.yui.calendar.DateTimeField;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextArea;
-import org.apache.wicket.markup.html.form.TextField;
 
 
 /**
@@ -26,40 +19,7 @@ public class AddGroupPage extends WebPage {
     public AddGroupPage(final PageReference modalWindowPage,
                         final ModalWindow window) {
 
-        Form form = new Form("addForm");
-
-        form.add(new Label("addGroupHeader", "Create group"));
-
-        form.add(new TextField<String>("groupName").setRequired(true));
-        form.add(new TextArea<String>("groupDesc").setRequired(true));
-
-        form.add(
-                new AjaxButton("submitForm", form) {
-                    @Override
-                    protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                        window.close(target);
-                    }
-                }.add(new AjaxEventBehavior("onclick") {
-                    @Override
-                    protected void onEvent(AjaxRequestTarget target) {
-                        window.close(target);
-                    }
-                })
-        );
-
-        form.add(
-                new AjaxButton("closeForm", form) {
-                    @Override
-                    protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                        window.close(target);
-                    }
-                }.add(new AjaxEventBehavior("onclick") {
-                    @Override
-                    protected void onEvent(AjaxRequestTarget target) {
-                        window.close(target);
-                    }
-                })
-        );
+        Form form = new GroupForm("addForm", window);
 
         add(form);
     }
