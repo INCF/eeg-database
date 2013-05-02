cbd217c5c86d7579f50c5e4e9bda4bcb4576f85b
Jakub Danek <jakub.danek@yoso.fi>
2013-05-02 19:20:01 +0200

    DataProvider generic implementation extracted.

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/ListExperimentsDataProvider.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/ListExperimentsDataProvider.java
@@ -1,23 +1,14 @@
 package cz.zcu.kiv.eegdatabase.wui.ui.experiments;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
-import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 
 import cz.zcu.kiv.eegdatabase.data.pojo.Experiment;
 import cz.zcu.kiv.eegdatabase.data.pojo.Person;
+import cz.zcu.kiv.eegdatabase.wui.components.repeater.BasicDataProvider;
 import cz.zcu.kiv.eegdatabase.wui.core.experiments.ExperimentsFacade;
-import java.util.ArrayList;
 
 /**
  * Dataprovider implementation used in table on ListExperimentPage.
@@ -25,15 +16,7 @@ import java.util.ArrayList;
  * @author Jakub Rinkes
  *
  */
-public class ListExperimentsDataProvider extends SortableDataProvider<Experiment, String> {
-
-    private static final long serialVersionUID = -2474056753967062150L;
-
-    protected Log log = LogFactory.getLog(getClass());
-
-    ExperimentsFacade facade;
-
-    private IModel<List<Experiment>> listModel = new Model(new ArrayList());
+public class ListExperimentsDataProvider extends BasicDataProvider<Experiment> {
 
     /**
      * Specialized constructor which loads experiments based on given properties
@@ -43,34 +26,31 @@ public class ListExperimentsDataProvider extends SortableDataProvider<Experiment
      * @param subject  search by isSubject (related to the person parameter)
      */
     public ListExperimentsDataProvider(ExperimentsFacade facade, Person person, boolean owner, boolean subject) {
+		super("experimentId", SortOrder.ASCENDING);
+		List<Experiment> list;
 
-	List<Experiment> list;
-        this.facade = facade;
-        setSort("experimentId", SortOrder.ASCENDING);
+		int size;
+			if (owner) {
+				size = facade.getCountForExperimentsWhereOwner(person);
+				list = facade.getExperimentsWhereOwner(person, (int) 0, size);
+			} else if (subject) {
+				size = facade.getCountForExperimentsWhereSubject(person);
+				list = facade.getExperimentsWhereSubject(person, (int) 0, size);
+			} else {
+				size = facade.getCountForAllExperimentsForUser(person);
+				list = facade.getAllExperimentsForUser(person, (int) 0, size);
+			}
 
-	int size;
-        if (owner) {
-            size = facade.getCountForExperimentsWhereOwner(person);
-            list = facade.getExperimentsWhereOwner(person, (int) 0, size);
-        } else if (subject) {
-            size = facade.getCountForExperimentsWhereSubject(person);
-            list = facade.getExperimentsWhereSubject(person, (int) 0, size);
-        } else {
-            size = facade.getCountForAllExperimentsForUser(person);
-            list = facade.getAllExperimentsForUser(person, (int) 0, size);
-        }
-
-	listModel.setObject(list);
+		super.listModel.setObject(list);
 
     }
 
-    /**
+	/**
      * Constructor which wraps a list of experiments.
      * @param experiments
      */
     public ListExperimentsDataProvider(List<Experiment> experiments) {
-	setSort("experimentId", SortOrder.ASCENDING);
-	this.listModel.setObject(experiments);
+		super(experiments, "experimentId", SortOrder.ASCENDING);
     }
 
     /**
@@ -80,57 +60,7 @@ public class ListExperimentsDataProvider extends SortableDataProvider<Experiment
      * @param listModel
      */
     public ListExperimentsDataProvider(IModel<List<Experiment>> listModel) {
-	setSort("experimentId", SortOrder.ASCENDING);
-	this.listModel = listModel;
-    }
-
-    @Override
-    public Iterator<? extends Experiment> iterator(long first, long count) {
-
-        if (getSort() != null)
-            Collections.sort(listModel.getObject(), new ExperimentsDataProviderComparator());
-
-        if (size() < first + count)
-            listModel.getObject().subList((int) first, (int) (first + size() - first)).iterator();
-
-        return listModel.getObject().subList((int) first, (int) (first + count)).iterator();
-    }
-
-    @Override
-    public long size() {
-        return listModel.getObject().size();
-    }
-
-    @Override
-    public IModel<Experiment> model(Experiment object) {
-        return new Model<Experiment>(object);
-    }
-
-    private class ExperimentsDataProviderComparator implements Comparator<Experiment>, Serializable {
-        
-        private static final long serialVersionUID = 1L;
-
-        @SuppressWarnings("unchecked")
-        public int compare(final Experiment o1, final Experiment o2) {
-            PropertyModel<Comparable> model1 = new PropertyModel<Comparable>(o1, getSort().getProperty());
-            PropertyModel<Comparable> model2 = new PropertyModel<Comparable>(o2, getSort().getProperty());
-
-            int result = 0;
-
-            if (model1.getObject() == null)
-                result = -1;
-            else if (model2.getObject() == null)
-                result = 1;
-            else if (model1.getObject() != null && model2.getObject() != null)
-                result = model1.getObject().compareTo(model2.getObject());
-
-            if (!getSort().isAscending()) {
-                result = -result;
-            }
-
-            return result;
-        }
-
+		super(listModel, "experimentId", SortOrder.ASCENDING);
     }
 
 }
