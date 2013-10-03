453c3c1fcb2d46b3808f95cc4ab276a4fd300a94
rinkesj <rinkesj@students.zcu.cz>
2013-10-03 23:35:42 +0200

    added licence header for java, xml, html, properties files in project

--- src/main/java/cz/zcu/kiv/eegdatabase/logic/controller/experiment/MetadataCommand.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/logic/controller/experiment/MetadataCommand.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * This file is part of the EEG-database project
+ * 
+ *   ==========================================
+ *  
+ *   Copyright (C) 2013 by University of West Bohemia (http://www.zcu.cz/en/)
+ *  
+ *  ***********************************************************************************************************************
+ *  
+ *   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *   the License. You may obtain a copy of the License at
+ *  
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *   an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ *  
+ *  ***********************************************************************************************************************
+ *  
+ *   MetadataCommand.java, 2013/10/02 00:01 Jakub Rinkes
+ ******************************************************************************/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
