de8a8e3b89144773c7839d00302cf71d6aceabdd
Jakub Krauz <jakub.krauz@volny.cz>
2014-02-18 21:01:53 +0100

    Added script creating table "form_layout".

---
+++ sql/scripts/alter/20140218_create_table_FORM_LAYOUT.sql
@@ -0,0 +1,11 @@
+CREATE TABLE form_layout (
+	form_layout_id INTEGER PRIMARY KEY NOT NULL,
+	form_name VARCHAR(50) NOT NULL,
+	layout_name VARCHAR(50) NOT NULL,
+	content OID NOT NULL,
+	person_id INTEGER
+);
+
+ALTER TABLE form_layout ADD CONSTRAINT form_layout_unique_idx UNIQUE (form_name, layout_name);
+
+ALTER TABLE form_layout ADD CONSTRAINT form_layout_person_fk FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE SET NULL;
