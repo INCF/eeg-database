39497d7182d1ca6db9d23de89720640d167737b7
rinkesj <rinkesj@students.zcu.cz>
2014-04-16 20:03:27 +0200

    fixed: bug in dataprovider when list size is smaller like count of page list item.

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/groups/ListResearchGroupsDataProvider.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/groups/ListResearchGroupsDataProvider.java
@@ -22,98 +22,38 @@
  ******************************************************************************/
 package cz.zcu.kiv.eegdatabase.wui.ui.groups;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
-import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 
 import cz.zcu.kiv.eegdatabase.data.pojo.ResearchGroup;
+import cz.zcu.kiv.eegdatabase.wui.components.repeater.BasicDataProvider;
 import cz.zcu.kiv.eegdatabase.wui.core.group.ResearchGroupFacade;
 
 /**
  * Dataprovider implementation used in table on ListResearchGroupPage.
  * 
  * @author Jakub Rinkes
- *
+ * 
  */
-public class ListResearchGroupsDataProvider extends SortableDataProvider<ResearchGroup, String> {
+public class ListResearchGroupsDataProvider extends BasicDataProvider<ResearchGroup> {
 
     private static final long serialVersionUID = -2474056753967062150L;
 
     protected Log log = LogFactory.getLog(getClass());
 
-    ResearchGroupFacade facade;
-
-    private List<ResearchGroup> list;
-
-    private int size;
-
     public ListResearchGroupsDataProvider(ResearchGroupFacade facade) {
+        super("title", SortOrder.ASCENDING);
 
-        this.facade = facade;
-        setSort("title", SortOrder.ASCENDING);
+        List<ResearchGroup> list;
+        int size;
 
         size = facade.getCountForList();
         list = facade.getGroupsForList(0, size);
 
-    }
-
-    @Override
-    public Iterator<? extends ResearchGroup> iterator(long first, long count) {
-
-        if (getSort() != null)
-            Collections.sort(list, new ResearchGroupsDataProviderComparator());
-
-        if (size() < first + count)
-            list.subList((int) first, (int) (first + size() - first)).iterator();
-
-        return list.subList((int) first, (int) (first + count)).iterator();
-    }
-
-    @Override
-    public long size() {
-        return size;
-    }
-
-    @Override
-    public IModel<ResearchGroup> model(ResearchGroup object) {
-        return new Model<ResearchGroup>(object);
-    }
-
-    private class ResearchGroupsDataProviderComparator implements Comparator<ResearchGroup>, Serializable {
-
-        private static final long serialVersionUID = 1L;
-
-        @SuppressWarnings("unchecked")
-        public int compare(final ResearchGroup o1, final ResearchGroup o2) {
-            PropertyModel<Comparable> model1 = new PropertyModel<Comparable>(o1, getSort().getProperty());
-            PropertyModel<Comparable> model2 = new PropertyModel<Comparable>(o2, getSort().getProperty());
-
-            int result = 0;
-
-            if (model1.getObject() == null)
-                result = -1;
-            else if (model2.getObject() == null)
-                result = 1;
-            else if (model1.getObject() != null && model2.getObject() != null)
-                result = model1.getObject().compareTo(model2.getObject());
-
-            if (!getSort().isAscending()) {
-                result = -result;
-            }
-
-            return result;
-        }
-
+        super.listModel.setObject(list);
     }
 
 }
--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/scenarios/ListScenariosDataProvider.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/scenarios/ListScenariosDataProvider.java
@@ -22,23 +22,16 @@
  ******************************************************************************/
 package cz.zcu.kiv.eegdatabase.wui.ui.scenarios;
 
-import java.io.Serializable;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
-import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 
 import cz.zcu.kiv.eegdatabase.data.pojo.Person;
 import cz.zcu.kiv.eegdatabase.data.pojo.Scenario;
 import cz.zcu.kiv.eegdatabase.wui.app.session.EEGDataBaseSession;
+import cz.zcu.kiv.eegdatabase.wui.components.repeater.BasicDataProvider;
 import cz.zcu.kiv.eegdatabase.wui.core.scenarios.ScenariosFacade;
 
 /**
@@ -47,7 +40,7 @@ import cz.zcu.kiv.eegdatabase.wui.core.scenarios.ScenariosFacade;
  * @author Jakub Rinkes
  *
  */
-public class ListScenariosDataProvider extends SortableDataProvider<Scenario, String> {
+public class ListScenariosDataProvider extends BasicDataProvider<Scenario> {
 
     private static final long serialVersionUID = 3555979400074686801L;
 
@@ -55,67 +48,18 @@ public class ListScenariosDataProvider extends SortableDataProvider<Scenario, St
 
     ScenariosFacade facade;
 
-    private List<Scenario> list;
-
-    private int size;
 
     public ListScenariosDataProvider(ScenariosFacade facade) {
+        super("scenarioId", SortOrder.ASCENDING);
 
-        this.facade = facade;
-        setSort("scenarioId", SortOrder.ASCENDING);
-
+        List<Scenario> list;
+        int size;
+        
         Person loggedUser = EEGDataBaseSession.get().getLoggedUser();
         size = facade.getScenarioCountForList(loggedUser);
         list = facade.getScenariosForList(loggedUser, 0, size);
-
+        
+        super.listModel.setObject(list);
     }
 
-    @Override
-    public Iterator<? extends Scenario> iterator(long first, long count) {
-
-        if (getSort() != null)
-            Collections.sort(list, new ScenariosDataProviderComparator());
-
-        if (size() < first + count)
-            list.subList((int) first, (int) (first + size() - first)).iterator();
-
-        return list.subList((int) first, (int) (first + count)).iterator();
-    }
-
-    @Override
-    public long size() {
-        return size;
-    }
-
-    @Override
-    public IModel<Scenario> model(Scenario object) {
-        return new Model<Scenario>(object);
-    }
-
-    private class ScenariosDataProviderComparator implements Comparator<Scenario>, Serializable {
-
-        private static final long serialVersionUID = 1L;
-
-        @SuppressWarnings("unchecked")
-        public int compare(final Scenario o1, final Scenario o2) {
-            PropertyModel<Comparable> model1 = new PropertyModel<Comparable>(o1, getSort().getProperty());
-            PropertyModel<Comparable> model2 = new PropertyModel<Comparable>(o2, getSort().getProperty());
-
-            int result = 0;
-
-            if (model1.getObject() == null)
-                result = -1;
-            else if (model2.getObject() == null)
-                result = 1;
-            else if (model1.getObject() != null && model2.getObject() != null)
-                result = model1.getObject().compareTo(model2.getObject());
-
-            if (!getSort().isAscending()) {
-                result = -result;
-            }
-
-            return result;
-        }
-
-    }
 }
