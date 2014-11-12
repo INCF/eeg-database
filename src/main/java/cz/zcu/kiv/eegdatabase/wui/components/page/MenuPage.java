8f02fb6d54956b8e6218917d7d140f85743fe0af
stebjan <stebjan@kiv.zcu.cz>
2014-11-12 16:18:34 +0100

    Year in footer is generated dynamically.

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/components/page/MenuPage.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/components/page/MenuPage.java
@@ -23,6 +23,7 @@
 package cz.zcu.kiv.eegdatabase.wui.components.page;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
@@ -124,6 +125,10 @@ public class MenuPage extends BasePage {
         add(new MainMenu("mainMenu"));
 
         add(new ExternalLink("footerLink", ResourceUtils.getString("general.footer.link"), ResourceUtils.getString("general.footer.link.title")));
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        add(new Label("year", ResourceUtils.getString("general.footer.year") + year));
     }
 
     public BaseFeedbackMessagePanel getFeedback() {
