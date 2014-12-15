8dcf69df609fa2e5e4f539dadfb22c03a8845de3
stebjan <stebjan@kiv.zcu.cz>
2014-12-15 08:52:00 +0100

    no-test-pom.xml is deleted, test cases for adding lists definitions are created.

---
+++ src/test/java/cz/zcu/kiv/eegdatabase/ui/AddArtifactTestIT.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * This file is part of the EEG-database project
+ *
+ * ==========================================
+ *
+ * Copyright (C) 2013 by University of West Bohemia (http://www.zcu.cz/en/)
+ *
+ * ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ***********************************************************************************************************************
+ *
+ * AddArtifactTestIT.java, 2014/12/13 00:01 Jan Stebetak
+ ******************************************************************************/
+package cz.zcu.kiv.eegdatabase.ui;
+
+import cz.zcu.kiv.eegdatabase.data.TestUtils;
+import cz.zcu.kiv.eegdatabase.data.dao.PersonDao;
+import cz.zcu.kiv.eegdatabase.data.pojo.Person;
+import cz.zcu.kiv.eegdatabase.logic.Util;
+import net.sourceforge.jwebunit.junit.WebTester;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Honza on 13.12.14.
+ */
+public class AddArtifactTestIT extends AbstractUITest {
+
+    private WebTester tester;
+
+    @Autowired
+    private PersonDao personDao;
+
+    @BeforeMethod(groups = "web")
+    public void setUp() {
+        if (!personDao.usernameExists("jan.stebetak@seznam.cz")) {
+            Person person = TestUtils.createPersonForTesting("jan.stebetak@seznam.cz", Util.ROLE_USER);
+            person.setConfirmed(true);
+            personDao.create(person);
+        }
+
+        tester = new WebTester();
+        // tester.setBaseUrl("http://eeg2.kiv.zcu.cz:8080");
+        tester.setBaseUrl(url);
+        tester.beginAt("/home-page");
+        tester.setTextField("userName", "jan.stebetak@seznam.cz");
+        tester.setTextField("password", "stebjan");
+        tester.clickButtonWithText("Log in");
+        tester.assertTextPresent("Log out");
+
+    }
+    @Test(groups = "web")
+    public void testArtifactValidation() throws InterruptedException {
+
+        createGroupIfNotExists();
+
+        tester.clickLinkWithText("Lists");
+        tester.assertLinkPresentWithText("Artifact definitions");
+        tester.clickLinkWithText("Artifact definitions");
+
+        tester.assertLinkPresentWithText("Add artifact definition");
+        tester.clickLinkWithText("Add artifact definition");
+
+        tester.setTextField("compensation", "");
+        tester.setTextField("rejectCondition", "");
+        tester.clickButtonWithText("Save");
+        Thread.sleep(waitForAjax);
+        tester.assertTextPresent("Field 'Compensation' is required.");
+        tester.assertTextPresent("Field 'Reject condition' is required.");
+
+        tester.clickLinkWithText("Log out");
+
+    }
+
+    @Test(groups = "web")
+    public void testAddArtifact() throws InterruptedException {
+
+        createGroupIfNotExists();
+
+        tester.clickLinkWithText("Lists");
+        tester.assertLinkPresentWithText("Artifact definitions");
+        tester.clickLinkWithText("Artifact definitions");
+
+        tester.assertLinkPresentWithText("Add artifact definition");
+        tester.clickLinkWithText("Add artifact definition");
+
+        tester.setTextField("compensation", "newCompensation");
+        tester.setTextField("rejectCondition", "newCondition");
+        tester.clickButtonWithText("Save");
+        Thread.sleep(waitForAjax);
+
+        tester.assertTextPresent("newCompensation");
+
+        tester.clickLinkWithText("Log out");
+
+    }
+
+    private void createGroupIfNotExists() throws InterruptedException {
+        tester.clickLinkWithText("Groups");
+        try {
+            tester.assertTextPresent("new group");
+        } catch (AssertionError ex) {
+            tester.clickLinkWithText("Create group");
+            tester.setTextField("title", "new group");
+            tester.setTextField("description", "description");
+            tester.clickButtonWithText("Save");
+            Thread.sleep(waitForAjax);
+        }
+    }
+}
