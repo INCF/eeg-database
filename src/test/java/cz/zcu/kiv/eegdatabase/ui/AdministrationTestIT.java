45e583c81e1e4fb0635568ff4ac2a4c1f50b92d0
stebjan <stebjan@kiv.zcu.cz>
2014-12-22 08:49:20 +0100

    Person test and Administration test created

---
+++ src/test/java/cz/zcu/kiv/eegdatabase/ui/AdministrationTestIT.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * This file is part of the EEG-database project
+ *
+ * ==========================================
+ *
+ * Copyright (C) 2013 by University of West Bohemia (http://www.zcu.cz/en/)
+ *
+ * ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ***********************************************************************************************************************
+ *
+ * AdministrationTestIT.java, 2014/12/19 00:01 Jan Stebetak
+ ******************************************************************************/
+package cz.zcu.kiv.eegdatabase.ui;
+
+import cz.zcu.kiv.eegdatabase.data.TestUtils;
+import cz.zcu.kiv.eegdatabase.data.dao.PersonDao;
+import cz.zcu.kiv.eegdatabase.data.pojo.Person;
+import cz.zcu.kiv.eegdatabase.logic.Util;
+import net.sourceforge.jwebunit.junit.WebTester;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Honza on 19.12.14.
+ */
+public class AdministrationTestIT extends AbstractUITest {
+
+    private WebTester tester;
+
+    @Autowired
+    private PersonDao personDao;
+
+    @BeforeMethod(groups = "web")
+    public void setUp() {
+        if (!personDao.usernameExists("jan.stebetak2@seznam.cz")) {
+            Person person = TestUtils.createPersonForTesting("jan.stebetak2@seznam.cz", Util.ROLE_ADMIN);
+            person.setConfirmed(true);
+            personDao.create(person);
+        }
+        if (!personDao.usernameExists("test@test.com")) {
+            personDao.create(TestUtils.createPersonForTesting("test@test.com", Util.ROLE_USER));
+        }
+
+        tester = new WebTester();
+        // tester.setBaseUrl("http://eeg2.kiv.zcu.cz:8080");
+        tester.setBaseUrl(url);
+        tester.beginAt("/home-page");
+        tester.setTextField("userName", "jan.stebetak2@seznam.cz");
+        tester.setTextField("password", "stebjan");
+        tester.clickButtonWithText("Log in");
+        tester.assertTextPresent("Log out");
+
+    }
+    @Test(groups = "web")
+    public void testManageAccounts() throws InterruptedException {
+
+
+        tester.clickLinkWithText("Administration");
+        tester.assertLinkPresentWithText("Manage user roles");
+        tester.clickLinkWithText("Manage user roles");
+        tester.setTextField("personField", "test@test.com");
+        tester.selectOption("authority", "ROLE_ADMIN");
+
+        Thread.sleep(waitForAjax);
+
+        tester.assertTextPresent("User role for user test@test.com changed to Administrator.");
+
+        tester.clickLinkWithText("Log out");
+
+    }
+}
