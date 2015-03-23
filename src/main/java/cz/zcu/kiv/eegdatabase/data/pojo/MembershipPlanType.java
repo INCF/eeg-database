ae6959ed23515b35b367c4da517bfad91c17ff2d
lichous <spilmer.l@seznam.cz>
2015-03-23 22:24:46 +0100

    MembershipPlan dao -MembershipPlanType enum added -MembershipPlanDao interface added -SimpleMembershipPlanDao class added

---
+++ src/main/java/cz/zcu/kiv/eegdatabase/data/pojo/MembershipPlanType.java
@@ -0,0 +1,40 @@
+package cz.zcu.kiv.eegdatabase.data.pojo;
+
+/**
+ * ********************************************************************************************************************
+ * <p/>
+ * This file is part of the eegdatabase project
+ * <p/>
+ * ==========================================
+ * <p/>
+ * Copyright (C) 2015 by University of West Bohemia (http://www.zcu.cz/en/)
+ * <p/>
+ * **********************************************************************************************************************
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * <p/>
+ * **********************************************************************************************************************
+ * <p/>
+ * MembershipPlanType, 2015/03/23 21:42 administrator
+ * <p/>
+ * ********************************************************************************************************************
+ */
+public enum MembershipPlanType {
+    /*
+    * MembershipPlan for persons
+    * */
+    PERSON,
+
+    /*
+    * MembershipPlan for groups
+    * */
+    GROUP
+
+}
