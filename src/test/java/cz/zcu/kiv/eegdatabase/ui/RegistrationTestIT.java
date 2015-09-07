b40450e05bd972ea5708d0ee43a4aaf4def3612e
stebjan <stebjan@kiv.zcu.cz>
2015-09-07 15:18:18 +0200

    gender removed from the registration test

--- src/test/java/cz/zcu/kiv/eegdatabase/ui/RegistrationTestIT.java
+++ src/test/java/cz/zcu/kiv/eegdatabase/ui/RegistrationTestIT.java
@@ -22,18 +22,12 @@
 ******************************************************************************/
 package cz.zcu.kiv.eegdatabase.ui;
 
-import com.thoughtworks.selenium.DefaultSelenium;
-import cz.zcu.kiv.eegdatabase.wui.ui.security.RegistrationPage;
 import junit.framework.Assert;
-import org.apache.wicket.util.tester.WicketTester;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
@@ -68,7 +62,6 @@ public class RegistrationTestIT extends AbstractUITest {
         assertTrue(driver.getPageSource().contains("Field 'E-mail' is required"));
         assertTrue(driver.getPageSource().contains("Field 'Password' is required"));
         assertTrue(driver.getPageSource().contains("Field 'Repeat password' is required"));
-        assertTrue(driver.getPageSource().contains("Field 'Gender' is required"));
         assertTrue(driver.getPageSource().contains("Field 'Control text' is required"));
         assertTrue(driver.getPageSource().contains("Field 'Education Level' is required"));
 
@@ -84,7 +77,6 @@ public class RegistrationTestIT extends AbstractUITest {
         Assert.assertEquals(driver.getTitle(), "Registration");
         driver.findElement(By.name("name")).sendKeys("testName");
         driver.findElement(By.name("surname")).sendKeys("testSurname");
-        driver.findElement(By.name("gender")).sendKeys("M");
         driver.findElement(By.name("email")).sendKeys("xxx@seznam");
         driver.findElement(By.name("password")).sendKeys("password");
         driver.findElement(By.name("passwordVerify")).sendKeys("password");
@@ -111,7 +103,6 @@ public class RegistrationTestIT extends AbstractUITest {
         driver.findElement(By.name("name")).sendKeys("testName");
         driver.findElement(By.name("surname")).sendKeys("testSurname");
         driver.findElement(By.name("dateOfBirth")).sendKeys("22/11/1998");
-        driver.findElement(By.name("gender")).sendKeys("M");
         driver.findElement(By.name("email")).sendKeys("xxx@xxx.com");
         driver.findElement(By.name("password")).sendKeys("password");
         driver.findElement(By.name("passwordVerify")).sendKeys("passwordxx");
