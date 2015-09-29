dcc78b1478d10b1e2dcdd895643856dd1930c45b
krauz <krauz@kiv.zcu.cz>
2015-09-29 13:40:30 +0200

    adjusted the "add license" modal window in the experiment creation form

--- src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/modals/AddLicensePage.java
+++ src/main/java/cz/zcu/kiv/eegdatabase/wui/ui/experiments/modals/AddLicensePage.java
@@ -1,9 +1,6 @@
 package cz.zcu.kiv.eegdatabase.wui.ui.experiments.modals;
 
-import com.sun.java.swing.plaf.windows.resources.windows;
-
 import cz.zcu.kiv.eegdatabase.data.pojo.Experiment;
-import cz.zcu.kiv.eegdatabase.data.pojo.ExperimentLicence;
 import cz.zcu.kiv.eegdatabase.data.pojo.License;
 import cz.zcu.kiv.eegdatabase.data.pojo.LicenseType;
 import cz.zcu.kiv.eegdatabase.wui.components.form.input.AjaxDropDownChoice;
@@ -11,7 +8,6 @@ import cz.zcu.kiv.eegdatabase.wui.components.utils.ResourceUtils;
 import cz.zcu.kiv.eegdatabase.wui.components.utils.WicketUtils;
 import cz.zcu.kiv.eegdatabase.wui.core.experimentLicense.ExperimentLicenseFacade;
 import cz.zcu.kiv.eegdatabase.wui.core.license.LicenseFacade;
-
 import org.apache.wicket.PageReference;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
@@ -19,22 +15,18 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.link.ResourceLink;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.*;
 import org.apache.wicket.request.resource.AbstractResource;
 import org.apache.wicket.request.resource.ByteArrayResource;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.util.lang.Classes;
 import org.apache.wicket.util.time.Duration;
-import org.apache.wicket.validation.validator.StringValidator;
-
-import java.io.ByteArrayInputStream;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,17 +34,17 @@ import java.util.List;
 /**
  * Created by Lichous on 16.5.15.
  */
-
 @AuthorizeInstantiation(value = { "ROLE_USER", "ROLE_EXPERIMENTER", "ROLE_ADMIN" })
 public class AddLicensePage extends WebPage {
+    
     private static final long serialVersionUID = 6965423677772732931L;
 
     public List<License> licenses;
 
+    
     public AddLicensePage(final PageReference modalWindowPage, final ModalWindow window, final IModel<Experiment> model) {
         licenses = new ArrayList<License>();
         add(new LicenseForm("addForm", window, model));
-
     }
 
     @Override
@@ -68,6 +60,7 @@ public class AddLicensePage extends WebPage {
     protected void onSubmitAction(IModel<License> model, Integer id, AjaxRequestTarget target, Form<?> form) {
     }
 
+    
     private class LicenseForm extends Form<License> {
 
         @SpringBean
@@ -76,13 +69,18 @@ public class AddLicensePage extends WebPage {
         @SpringBean
         ExperimentLicenseFacade experimentLicenseFacade;
 
-        private AjaxDropDownChoice<License> ddc;
-        protected IModel<License> licenseModel;
-        private AjaxButton saveButton;
-        private ResourceLink<Void> downloadLink;
-        //private FormComponent<BigDecimal> priceInput;
-        private Form<License> form;
         private ModalWindow window;
+        protected IModel<License> licenseModel;
+        
+        private Form<License> form;
+        private AjaxDropDownChoice<License> ddc;
+        private NumberTextField<BigDecimal> priceInput;
+        private AjaxButton saveButton;
+        
+        private WebMarkupContainer licenseDetails;
+        private ExternalLink licenseLink;
+        private ResourceLink<Void> downloadLink;
+        
 
         public LicenseForm(String id, final ModalWindow window, final IModel<Experiment> model) {
             super(id, new CompoundPropertyModel<License>(new License()));
@@ -96,61 +94,51 @@ public class AddLicensePage extends WebPage {
         }
 
         private void addFormFields() {
-
-            /*Label l = new Label("attachmentFileName", new PropertyModel<String>(licenseModel, "attachmentFileName"));
-            add(l);*/
-
-            /*priceInput = new NumberTextField<BigDecimal>("price", new PropertyModel<BigDecimal>(licenseModel, "price"), BigDecimal.class).setMinimum(BigDecimal.ZERO);
+            //priceInput = new NumberTextField<BigDecimal>("price", new PropertyModel<BigDecimal>(licenseModel, "price"), BigDecimal.class);
+            priceInput = new NumberTextField<BigDecimal>("price");
+            priceInput.setModel(new Model<BigDecimal>(BigDecimal.ZERO));
+            priceInput.setMinimum(BigDecimal.ZERO);
             priceInput.setRequired(true);
             priceInput.setEnabled(false);
             priceInput.setLabel(ResourceUtils.getModel("label.license.price"));
-            add(priceInput);*/
+            add(priceInput);
 
             WicketUtils.addLabelsAndFeedback(this);
-
-            /*ByteArrayResource res;
+        }
+        
+        
+        private void addLicenseDetails() {
+            licenseDetails = new WebMarkupContainer("licenseDetails");
+            licenseDetails.setVisible(licenseModel.getObject() != null);
+            add(licenseDetails);
+            
+            Label title = new Label("title", new PropertyModel<String>(licenseModel, "title"));
+            licenseDetails.add(title);
+            
+            Label type = new Label("type", new PropertyModel<String>(licenseModel, "licenseType"));
+            licenseDetails.add(type);
+            
+            Label description = new Label("description", new PropertyModel<String>(licenseModel, "description"));
+            licenseDetails.add(description);
+            
+            PropertyModel<String> linkModel = new PropertyModel<String>(licenseModel, "link");
+            licenseLink = new ExternalLink("link", linkModel, linkModel);
+            licenseLink.setVisible(linkModel.getObject() != null);
+            licenseDetails.add(licenseLink);
+            
+            ByteArrayResource res;
             res = new ByteArrayResource("") {
                 @Override
                 public void configureResponse(final AbstractResource.ResourceResponse response, final IResource.Attributes attributes) {
                     response.setCacheDuration(Duration.NONE);
                 }
             };
-            downloadLink = new ResourceLink<Void>("download", res);
+            downloadLink = new ResourceLink<Void>("fileDownload", res);
             downloadLink.setVisible(false);
-
-            add(downloadLink);*/
-        }
-        
-        
-        private void addLicenseDetails() {
-            Label title = new Label("title", new PropertyModel<String>(licenseModel, "title"));
-            add(title);
+            licenseDetails.add(downloadLink);
             
-            Label type = new Label("type", new PropertyModel<String>(licenseModel, "licenseType"));
-            add(type);
-            
-            Label description = new Label("description", new PropertyModel<String>(licenseModel, "description"));
-            add(description);
-            
-            // FIXME nezobrazuje se
-            PropertyModel<String> linkModel = new PropertyModel<String>(licenseModel, "link");
-            ExternalLink link = new ExternalLink("link", linkModel, linkModel);
-            link.setVisible(linkModel.getObject() != null);
-            add(link);
-            
-            /*add(new Label("attachmentFileName", license.getAttachmentFileName()));
-
-            boolean isContent = license != null && license.getAttachmentFileName() != null;
-
-            ByteArrayResource res;
-            if (isContent) {
-                res = new ByteArrayResource("", licenseFacade.getLicenseAttachmentContent(license.getLicenseId()), license.getAttachmentFileName());
-            } else {
-                res = new ByteArrayResource("");
-            }
-            ResourceLink<Void> downloadLink = new ResourceLink<Void>("download", res);
-            downloadLink.setVisible(isContent);
-            add(downloadLink);*/
+            Label l = new Label("fileName", new PropertyModel<String>(licenseModel, "attachmentFileName"));
+            downloadLink.add(l);
         }
 
 
@@ -158,9 +146,7 @@ public class AddLicensePage extends WebPage {
             saveButton = new AjaxButton("submitButton", ResourceUtils.getModel("button.save")) {
                 @Override
                 protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                    //System.out.println("---LIC_TEM_ID: "+ddc.getModel().getObject().getLicenseId());
                     onSubmitAction(licenseModel, ddc.getModel().getObject().getLicenseId(), target, form);
-
                     target.add(form);
                 }
 
@@ -227,46 +213,45 @@ public class AddLicensePage extends WebPage {
                     if (option == null || option.getLicenseId() == 0) {
                         licenseModel.setObject(new License());
                         saveButton.setVisibilityAllowed(false);
-                        //priceInput.setEnabled(false);
+                        priceInput.setEnabled(false);
+                        licenseDetails.setVisible(false);
                         downloadLink.setVisible(false);
-
-
-                        // TODO kuba licence
                     } else {
-                        //if (option.getLicenseType()== LicenseType.COMMERCIAL /*&& option.getPrice().intValue() == 0*/) {
-                        //    priceInput.setEnabled(true);
-                        //} else {
-                        //    priceInput.setEnabled(false);
-                        //}
+                        
+                        // TODO kuba licence
+                        priceInput.setEnabled(option.getLicenseType() == LicenseType.COMMERCIAL);
+                        
                         License l = new License();
                         l.setTitle(option.getTitle());
                         l.setDescription(option.getDescription());
                         l.setLicenseType(option.getLicenseType());
+                        l.setLink(option.getLink());
                         //l.setPrice(option.getPrice());
                         l.setAttachmentFileName(option.getAttachmentFileName());
                         l.setTemplate(false);
                         l.setFileContentStream(null);
+                        
+                        licenseDetails.setVisible(true);
+                        licenseLink.setVisible(l.getLink() != null);
 
-
-                        if (option.getAttachmentFileName()!= null) {
+                        if (option.getAttachmentFileName() != null) {
                             ByteArrayResource res;
                             res = new ByteArrayResource("", licenseFacade.getLicenseAttachmentContent(option.getLicenseId()), option.getAttachmentFileName()) {
-
                                 @Override
                                 public void configureResponse(final AbstractResource.ResourceResponse response, final IResource.Attributes attributes) {
                                     response.setCacheDuration(Duration.NONE);
                                     response.setFileName(option.getAttachmentFileName());
                                 }
                             };
-
-                            ResourceLink<Void> newLink = new ResourceLink<Void>("download", res);
-
-                            downloadLink = (ResourceLink<Void>) downloadLink.replaceWith(newLink);
+                            ResourceLink<Void> newLink = new ResourceLink<Void>("fileDownload", res);
+                            newLink.add(new Label("fileName", option.getAttachmentFileName()));
+                            downloadLink.replaceWith(newLink);
+                            downloadLink = newLink;
                             downloadLink.setVisible(true);
-
                         } else {
                             downloadLink.setVisible(false);
                         }
+                        
                         licenseModel.setObject(l);
                         saveButton.setVisibilityAllowed(true);
                     }
