4f25a65505cc3f6f3c8495870861802f6a639197
jezekpetr <jezekp@kiv.zcu.cz>
2018-09-10 15:11:39 +0200

    added missing classes

---
+++ src/main/java/cz/zcu/kiv/eegdatabase/webservices/rest/metadata/wrappers/ExperimentData.java
@@ -0,0 +1,63 @@
+package cz.zcu.kiv.eegdatabase.webservices.rest.metadata.wrappers;
+
+import cz.zcu.kiv.eegdatabase.webservices.rest.common.utils.AdapterCDATA;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+/***********************************************************************************************************************
+ *
+ * This file is part of the eegdatabase project
+
+ * ==========================================
+ *
+ * Copyright (C) 2018 by University of West Bohemia (http://www.zcu.cz/en/)
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************
+ *
+ * ExperimentData, 2018/09/05 09:59 petr-jezek
+ *
+ **********************************************************************************************************************/
+@XmlRootElement(name = "ExperimentData")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ExperimentData {
+
+    public ExperimentData(String metadata, int experimentId) {
+        setExperimentId(experimentId);
+        setMetadata(metadata);
+    }
+
+    @XmlJavaTypeAdapter(AdapterCDATA.class)
+    private String metadata;
+
+    private int experimentId;
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
+
+    public int getExperimentId() {
+        return experimentId;
+    }
+
+    public void setExperimentId(int experimentId) {
+        this.experimentId = experimentId;
+    }
+}
